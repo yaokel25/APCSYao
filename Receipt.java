eb742e3a29333acd491c2b30cd497f8197775498
yaokel25 <yaokel25@7hills.org>
2023-09-01 12:34:01 -0400

    finished prompts and started making Receipt

--- Receipt.java
+++ Receipt.java
@@ -7,14 +7,25 @@ public class Receipt{
          int banana;
          int pear;
          int orange;
+         String end;
 
          Scanner scan = new Scanner(System.in);
          System.out.println("Welcome to the store! We sell apples, bananas, pears, and oranges.");
          
+         //prompt for number of objects
          System.out.println("How many apples would you like? ($1.50 each) ");
          apple = scan.nextInt(); 
          System.out.println("How many bananas would you like? ($0.75 each)");
          banana = scan.nextInt();
+         System.out.println("How many pears would you like? ($1.00 each)");
+         pear = scan.nextInt();
+         System.out.println("How many oranges would you like? ($1.00 each)");
+         orange = scan.nextInt();
+         System.out.println("Are you finished with you purchase?");
+         end = scan.nextLine();
+
+         System.out.println("****************************************");
+         System.out.println(apple + "Apple(s)" + "                   $1.50 each" + "$"apple*1.5);
 
         
     }
