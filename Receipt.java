729fe947f44042886885fce7b684ad0c23ee0c23
yaokel25 <yaokel25@7hills.org>
2023-09-01 12:16:47 -0400

    made variables and started prompts for # of things

--- Receipt.java
+++ Receipt.java
@@ -1,10 +1,22 @@
 //Receipt.java
-
 import java.util.Scanner;
 public class Receipt{
     public static void main(String[] args) {
+        //declare variables 
+         int apple;
+         int banana;
+         int pear;
+         int orange;
 
-        System.out.print("Hello World")
+         Scanner scan = new Scanner(System.in);
+         System.out.println("Welcome to the store! We sell apples, bananas, pears, and oranges.");
+         
+         System.out.println("How many apples would you like? ($1.50 each) ");
+         apple = scan.nextInt(); 
+         System.out.println("How many bananas would you like? ($0.75 each)");
+         banana = scan.nextInt();
+
+        
     }
 
 }
